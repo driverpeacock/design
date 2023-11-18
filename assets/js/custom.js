4e3a4fcd6c108e47297360645863e1ef91a573a1
driverpeacock <149901828+driverpeacock@users.noreply.github.com>
2023-11-19 02:22:59 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,66 @@
+//Fleet-silder js
+ $('#fleet-silder').owlCarousel({
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:6000,
+    navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>'],
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+//service-silder js
+
+ $('#service-silders').owlCarousel({
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:6000,
+    navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>'],
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+//service-silder js
+
+ $('#testimonial-silder').owlCarousel({
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:6000,
+    navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>'],
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+
